5d486fe1cc1bbfd8f81b600f2f8b04b3348ba0f0
rjrosenkoetter <rosenkoetterryan@gmail.com>
2023-12-24 01:35:38 -0600

    v0.1 - opening version log added

--- MLBSimulator/Program.cs
+++ MLBSimulator/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Linq.Expressions;
+
+string version = "0.1";
+DateTime lastUpdated = new DateTime(2023, 12, 24);
+
+static void printVer(string ver, DateTime lastUpdate)
+{
+    Console.WriteLine("MLB Simulator Version v" + ver);
+    Console.WriteLine("Last updated: " + lastUpdate.ToString("MMMM dd, yyyy"));
+    Console.WriteLine("- Created by Ryan Rosenkoetter.");
+}
+
+printVer(version, lastUpdated);
--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # MLBSim
-# Version Info: v0.0 - pre-alpha
+# Version Info: v0.1 - pre-alpha
 # A simulator of a Major League Baseball season. The project is currently in pre-alpha.
 # Further information on usage will be written when V1 of the simulator is released.
